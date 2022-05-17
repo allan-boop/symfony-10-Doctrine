23bf12740fa8d62899e68715da03bb0bf01fda3e
allan <allan.hans68350@gmail.com>
2022-05-17 21:34:59 +0200

    navbar

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+require('bootstrap');
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -6,7 +7,11 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+
+
+console.log('Hello Webpack Encore !')
